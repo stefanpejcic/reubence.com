7e2480c000bbdda65c158a50382ded90dc47e05c
reubence <reuben3699@gmail.com>
2022-05-24 09:55:10 +0530

    Alpha - Edits Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight  text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          <p className="hidden text-lg leading-7 text-gray-500 dark:text-gray-400 sm:flex">
             Eternally Seeking The Truth — sometimes by Learning, sometimes by Sharing, sometimes by
             chasing Ambitious Goals.{' '}
           </p>
